6246b550317babfac8305a2ef322df0a6789c080
Shota Amano <amasho@dev-cycle.io>
2020-05-30 10:03:12 +0900

    fix

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="w-full mt-10 ml-5">
-    <ul class="flex justify-around mb-10">
+  <div class="w-1/2 mt-10 ml-5">
+    <ul class="flex justify-evenly">
       <li class="font-sans text-xl">1. ほげほげ</li>
       <li class="font-sans text-xl">2. ふがふが</li>
       <li class="font-sans text-xl">3. もえもえ</li>
+      <li class="font-sans text-xl">4. グエーー</li>
     </ul>
     <div class="flex justify-center">
       <button
-        class="w-24 h-8 px-22 border border-solid bg-custom-117fa4 text-white rounded-lg outline-none"
+        class="w-24 h-8 mt-2 px-22 border border-solid bg-custom-117fa4 text-white rounded-lg outline-none"
       >
         ボタン
       </button>
